f591e62f71ac82f0fa741768fb56960556f70b63
Rupal Shah <rupsamshah@gmail.com>
2023-07-13 21:23:00 -0400

    updated code

--- static/js/birth.js
+++ static/js/birth.js
@@ -6,49 +6,7 @@ const eduLevelUrl = "http://127.0.0.1:5000/api/v1.0/eduLevel";
 const state_url = "http://127.0.0.1:5000/api/v1.0/state";
 const gender_url = "http://127.0.0.1:5000/api/v1.0/gender";
 
-d3.json(gender_url).then(function (data) {
-    let male_x = data.male_x;
-    let male_y = data.male_y;
-    let female_x = data.female_x;
-    let female_y = data.female_y;
-  
-    let maleBarTrace = {
-      x: male_x,
-      y: male_y,
-      name: "Male",
-      type: "bar",
-      marker: {
-        color: "#b8e6f2"
-      }
-    };
-  
-    let femaleBarTrace = {
-      x: female_x,
-      y: female_y,
-      name: "Female",
-      type: "bar",
-      marker: {
-        color:"#faa4a7"
-      }
-    };
-  
-    let barData = [maleBarTrace, femaleBarTrace];
-  
-    let barLayout = {
-      title: "Gender",
-      barmode: "group",
-      xaxis: {
-        tickmode: "linear"
-      }
-    };
-  
-    let responsive = {
-      responsive: true
-    };
-  
-    Plotly.newPlot("genderBarChart", barData, barLayout, responsive);
-  });
-  
+
 
 // Fetch the JSON data and console log it
 d3.json(eduLevelUrl).then(function (edu_data) {
@@ -90,8 +48,6 @@ function optionChanged(meta) {
   buildMetaData(meta);
 }
 
-
-
 // Populate dropdown options on page load
 d3.json("http://127.0.0.1:5000/api/v1.0/eduLevel").then(function (edu_data) {
   let dropdown = d3.select("#selDataset");
@@ -107,5 +63,72 @@ d3.json("http://127.0.0.1:5000/api/v1.0/eduLevel").then(function (edu_data) {
   let initialSelection = edu_data[0];
   buildMetaData(initialSelection);
 });
+
+// Fetch the state data from the API endpoint
+d3.json("/api/v1.0/state").then(function (data) {
+  // Print the received data in the console
+  console.log(data);
+
+  // Select the panel body element to display the state information
+  let panelBody = d3.select("#sample-metadata");
+
+  // Clear existing content
+  panelBody.html("");
+
+  // Loop through the data and display the state information
+  data.forEach(function (state) {
+    Object.entries(state).forEach(function ([key, value]) {
+      panelBody.append("p").text(`${key}: ${value}`);
+    });
+    panelBody.append("hr"); // Add a horizontal line between state information
+  });
+});
+
+
+
+d3.json(gender_url).then(function (data) {
+  let male_x = data.male_x;
+  let male_y = data.male_y;
+  let female_x = data.female_x;
+  let female_y = data.female_y;
+
+  let maleBarTrace = {
+    x: male_x,
+    y: male_y,
+    name: "Male",
+    type: "bar",
+    marker: {
+      color: "#b8e6f2"
+    }
+  };
+
+  let femaleBarTrace = {
+    x: female_x,
+    y: female_y,
+    name: "Female",
+    type: "bar",
+    marker: {
+      color:"#faa4a7"
+    }
+  };
+
+  let barData = [maleBarTrace, femaleBarTrace];
+
+  let barLayout = {
+    title: "Gender",
+    barmode: "group",
+    xaxis: {
+      tickmode: "linear"
+    }
+  };
+
+  let responsive = {
+    responsive: true
+  };
+
+  Plotly.newPlot("genderBarChart", barData, barLayout, responsive);
+});
+
+
 
 
