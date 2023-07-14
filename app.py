b2dabb663d814b0fbc05915614580cfb27ee0394
Rupal Shah <rupsamshah@gmail.com>
2023-07-13 20:27:20 -0400

    updated code

--- app.py
+++ app.py
@@ -13,27 +13,10 @@ from json import dump
 import json
 import numpy as np
 
-#################################################
-# Database Setup
-#################################################
-## engine = create_engine("sqlite:///data/USbirths.sqlite")
 engine = create_engine("sqlite:///sql_files/us_births.sqlite")
 test = pd.read_sql("select * from EightStatesData", con=engine.raw_connection())
 print(test)
 
-
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(autoload_with=engine)
-
-# print(Base.classes.keys())
-# # Save reference to the table
-# Births = Base.classes.EightStatesData
-
-##############################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
 
@@ -51,20 +34,47 @@ def state():
 
 @app.route("/api/v1.0/gender")
 def gender():
-
     df2 = test.groupby(['Year', 'Gender'])
-    return df2['Births'].agg('sum').to_json()
+    result = df2['Births'].agg('sum').reset_index()
+
+    male_data = result[result['Gender'] == 'M']
+    female_data = result[result['Gender'] == 'F']
+
+    male_x = male_data['Year']
+    male_y = male_data['Births']
+
+    female_x = female_data['Year']
+    female_y = female_data['Births']
+
+    data = {
+        'male_x': male_x.tolist(),
+        'male_y': male_y.tolist(),
+        'female_x': female_x.tolist(),
+        'female_y': female_y.tolist()
+    }
+
+    return jsonify(data)
+
+    plot.bar(x='Gender', ylabel='Number of Births', title='Total number of births group by year and gender', color=['blue', 'green'])
+
 
 
 @app.route("/api/v1.0/eduLevel")
 def education():
-
-    edu = list(test.EduLevel.unique())
-    print(edu)
+    edu = list(test["EduLevel"].unique())
     return jsonify(edu)
 
-    #plot.bar(x='Gender', ylabel='Number of Births', title='Total number of births group by year and gender', color=['blue', 'green'])
+@app.route("/api/v1.0/eduLevel/<edu_level>")
+def education_level(edu_level):
+    filtered_data = test[test["EduLevel"] == edu_level]
+    metadata = filtered_data.to_dict("records")
+    return jsonify(metadata)
 
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
--- static/js/birth.js
+++ static/js/birth.js
@@ -1,85 +1,111 @@
-// connect the console log to the javascript file
-console.log("birth.js")
+// Connect the console log to the JavaScript file
+console.log("birth.js");
 
-// Place url in a constant variable
-const url = "http://127.0.0.1:5000/api/v1.0/eduLevel"
-const gender_url = "http://127.0.0.1:5000/api/v1.0/gender"
-
+// Place URL in constant variables
+const eduLevelUrl = "http://127.0.0.1:5000/api/v1.0/eduLevel";
+const state_url = "http://127.0.0.1:5000/api/v1.0/state";
+const gender_url = "http://127.0.0.1:5000/api/v1.0/gender";
 
 d3.json(gender_url).then(function (data) {
-    console.log(data)
-})
-
-
-// Fetch the JSON data and console log it 
-d3.json(url).then(function (data) {
-    console.log(data);
-
-    // assign a variable to access the data
-    let elom = data
-    //iterate over the array to create indexes
-    data.forEach(function(row, index) {
-        row.index = index;
-      });
-     
-    // console log the new array
-    console.log(data);
-    
-    //
-    // create a function to return only North Carolina data
-    function northCarolina(state) {
-        return state.State == "North Carolina";
-    }
-    // call the custom function with a filter
-    let stateInData = data.filter(northCarolina);
-
-    //Display the result in console
-    console.log(stateInData);
-    //
-
-    // assign a variable to access the Education Level data
-    let elom = data.EduLevel
-
-    // assign a variable to access the HTML tag for the dropdown menu location
-    dropdown = d3.select("#selDataset")
-
-    // for loop to iterate through the values and display the index of choice
-    for (let i = 0; i < data.length; i++) {
-        dropdown
-            .append("option")
-            .text(data[i])
-            .property("value", data[i]);
+    let male_x = data.male_x;
+    let male_y = data.male_y;
+    let female_x = data.female_x;
+    let female_y = data.female_y;
+  
+    let maleBarTrace = {
+      x: male_x,
+      y: male_y,
+      name: "Male",
+      type: "bar",
+      marker: {
+        color: "#b8e6f2"
+      }
     };
+  
+    let femaleBarTrace = {
+      x: female_x,
+      y: female_y,
+      name: "Female",
+      type: "bar",
+      marker: {
+        color:"#faa4a7"
+      }
+    };
+  
+    let barData = [maleBarTrace, femaleBarTrace];
+  
+    let barLayout = {
+      title: "Gender",
+      barmode: "group",
+      xaxis: {
+        tickmode: "linear"
+      }
+    };
+  
+    let responsive = {
+      responsive: true
+    };
+  
+    Plotly.newPlot("genderBarChart", barData, barLayout, responsive);
+  });
+  
 
-    buildMetaData(data[0])
+// Fetch the JSON data and console log it
+d3.json(eduLevelUrl).then(function (edu_data) {
+  d3.json(state_url).then(function(state_data) {
+    console.log(eduLevelUrl); // Log edu_data to console
+    console.log(state_data); // Log state_data to console
+
+    // Assign a variable to access the HTML tag for the dropdown menu location
+    dropdown = d3.select("#selDataset");
+
+    // For loop to iterate through the values and display the index of choice
+    dropdown.html(""); // Clear existing options
+
+// For loop to iterate through the values and display the index of choice
+    for (let i = 0; i < edu_data.length; i++) {
+      dropdown
+    .append("option")
+    .text(edu_data[i])
+    .property("value", edu_data[i]);
+}
+  });
 });
 
-// create a function to populate sample metadata class with info
 function buildMetaData(id) {
-    d3.json(url).then(function (data) {
-        let meta = data.EduLevelCode
+  const url = `${eduLevelUrl}/${id}`;
 
-        let resultArray = data.filter(sampleObj => sampleObj.data == data);
-        console.log("resultArray")
-        // console.log(resultArray)
+  d3.json(url).then(function (data) {
+    let result = data[0];
+    let box = d3.select("#sample-metadata");
+    box.html("");
 
-        // assign result variable to grab the first index of the resulting array
-        let result = resultArray[0];
+    Object.entries(result).forEach(([key, value]) => {
+      box.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  });
+}
 
-        // assign a box variable where the data will display
-        let box = d3.select("#sample-metadata");
+function optionChanged(meta) {
+  buildMetaData(meta);
+}
 
-        // clears the metadata in the console log so it can be filled with the next pull
-        box.html("");
-        
-        // loop for the display box and format to uppercase
-        Object.keys(result).forEach((key) => {
-            box.append("h6").text(key.toUpperCase() + ":" + result[key]);
-           
-          });
-    })
-};
 
-function optionChanged(meta){
-    buildMetaData(meta)
-};
+
+// Populate dropdown options on page load
+d3.json("http://127.0.0.1:5000/api/v1.0/eduLevel").then(function (edu_data) {
+  let dropdown = d3.select("#selDataset");
+
+  edu_data.forEach((edu) => {
+    dropdown
+      .append("option")
+      .text(edu)
+      .property("value", edu);
+  });
+
+  // Trigger metadata population for the initial selection
+  let initialSelection = edu_data[0];
+  buildMetaData(initialSelection);
+});
+
+
