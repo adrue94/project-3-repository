74236db35e6b8a81813067b1e2c2a804041440bb
MDCummings86 <mdcummings@michaels-mbp.lan>
2023-07-16 09:22:18 -0400

    fixed dropdown for pie chart

--- static/js/weight.js
+++ static/js/weight.js
@@ -1,8 +1,8 @@
 // Begin code for Pie Chart and associated Dropdown
 
 // connect the console log to the javascript file
-console.log("weight.js")
-console.log("data.js")
+console.log("weight.js");
+console.log("data.js");
 
 // Create an array of each state's data
 let alaska = Object.values(data.alaska);
@@ -14,25 +14,29 @@ let newYork = Object.values(data.newYork);
 let northCarolina = Object.values(data.northCarolina);
 let texas = Object.values(data.texas);
 
+// log to console
+console.log(data);
+
 // Create an array of category labels
 let labels = Object.keys(data.alaska);
 
 // Display the default plot
 function init() {
-  let data = [{
+  let plotData = [{
     values: alaska,
     labels: labels,
-    marker: {colors: [
-      'rgb(0, 20, 200)',  
-      'rgb(40, 60, 240)',  
-      'rgb(80, 100, 40)',  
-      'rgb(120, 30, 80)',  
-      'rgb(160, 70, 120)',  
-      'rgb(200, 110, 160)',  
-      'rgb(70, 150, 175)',  
-      'rgb(50, 60, 130)' 
-    ]},
-    
+    marker: {
+      colors: [
+        'rgb(0, 20, 200)',
+        'rgb(40, 60, 240)',
+        'rgb(80, 100, 40)',
+        'rgb(120, 30, 80)',
+        'rgb(160, 70, 120)',
+        'rgb(200, 110, 160)',
+        'rgb(70, 150, 175)',
+        'rgb(50, 60, 130)'
+      ]
+    },
     type: "pie"
   }];
 
@@ -41,51 +45,45 @@ function init() {
     width: 800
   };
 
-  Plotly.newPlot("pie", data, layout);
+  Plotly.newPlot("pie", plotData, layout);
 }
 
 // On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", getData);
+dropdown = d3.selectAll("#selDatasetWeight").on("change", getData);
 
 // Function called by DOM changes
 function getData() {
-  let dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a letiable
+  let dropdownMenu = d3.select("#selDatasetWeight");
+  // Assign the value of the dropdown menu option to a variable
   let dataset = dropdownMenu.property("value");
   // Initialize an empty array for the state data
-  let data = [];
+  let selectedData = [];
 
-  if (dataset == 'alaska') {
-      data = alaska;
+  if (dataset === 'alaska') {
+    selectedData = alaska;
+  } else if (dataset === 'california') {
+    selectedData = california;
+  } else if (dataset === 'districtOfColumbia') {
+    selectedData = districtOfColumbia;
+  } else if (dataset === 'hawaii') {
+    selectedData = hawaii;
+  } else if (dataset === 'florida') {
+    selectedData = florida;
+  } else if (dataset === 'newYork') {
+    selectedData = newYork;
+  } else if (dataset === 'northCarolina') {
+    selectedData = northCarolina;
+  } else if (dataset === 'texas') {
+    selectedData = texas;
   }
-  else if (dataset == 'california') {
-      data = californa;
-  }
-  else if (dataset == 'districtOfColumbia') {
-      data = districtOfColumbia;
-  }
-  else if (dataset == 'hawaii') {
-    data = hawaii;
-  }
-  else if (dataset == 'florida') {
-    data = florida;
-  }
-  else if (dataset == 'newYork') {
-      data = newYork;
-  }
-  else if (dataset == 'northCarolina') {
-    data = northCarolina;
-  }
-  else if (dataset == 'texas') {
-    data = texas;
-  } 
-// Call function to update the chart
-  updatePlotly(data);
+
+  // Call function to update the chart
+  updatePlotly(selectedData);
 }
 
 // Update the restyled plot's values
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
+function updatePlotly(newData) {
+  Plotly.restyle("pie", "values", [newData]);
 }
 
 init();
